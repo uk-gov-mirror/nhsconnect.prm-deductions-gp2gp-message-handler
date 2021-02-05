922ee2c86f40e39c3bec884c0aca643f6acc893d
Rosie Amphlett <rosie.amphlett1@nhs.net>
2021-02-05 13:54:47 +0000

    [PRMT-1526] Added test_integration task to run only integration tests

--- src/test/java/uk/nhs/prm/deductions/gp2gpmessagehandler/Gp2gpMessageHandlerApplicationIntegrationTests.java
+++ src/test/java/uk/nhs/prm/deductions/gp2gpmessagehandler/Gp2gpMessageHandlerApplicationIntegrationTests.java
@@ -20,7 +20,7 @@ import static org.hamcrest.core.IsEqual.equalTo;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
-class Gp2gpMessageHandlerApplicationTests {
+class Gp2gpMessageHandlerApplicationIntegrationTests {
 	@Autowired
 	JmsTemplate jmsTemplate;
 
